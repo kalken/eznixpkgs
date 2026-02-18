29b5ed331d181a44b65ed1229a3292b22f3fb38e
kalken <linux4dummies@gmail.com>
2026-02-18 19:02:03 +0100

    updated default.nix in modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,19 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, inputs, ... }:
-
 {
-  imports = [ 
-    ./ezrouter.nix
-    ./eznetns.nix
-    ./prettysocks.nix
-  ];
-  
-  options.variables = lib.mkOption {
-    type = lib.types.attrs;
-    default = { };
-  };
-  config._module.args.variables = config.variables;
+  imports = builtins.concatLists (map
+    (f: [ (./. + "/${f}") ])
+    (builtins.filter
+      (name: name != "default.nix" && builtins.match ".*\\.nix" name != null)
+      (builtins.attrNames (builtins.readDir ./.))
+    )
+  );
 }
